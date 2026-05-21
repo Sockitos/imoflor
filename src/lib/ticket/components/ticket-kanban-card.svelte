4249f6ff07413fb58fedb0232c1bdf4b3a70bf6e
Tomás Figueiredo <tomasviana_cof@hotmail.com>
2026-05-21 16:04:20 +0100

    Add redirect logic to `Ticket` page to `ticket-kanban-card`

--- src/lib/ticket/components/ticket-kanban-card.svelte
+++ src/lib/ticket/components/ticket-kanban-card.svelte
@@ -3,11 +3,17 @@
 	import { Badge } from '@/shared/components/ui/badge';
 	import type { Ticket } from '@/ticket/types';
 	import { priorityMap } from './ticket-priority-map';
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
 
 	let { ticket }: { ticket: Ticket } = $props();
 
 	const priority = $derived(priorityMap[ticket.priority]);
 	const formattedDate = $derived(ticket.date.split('T')[0].split('-').reverse().join('/'));
+
+	function handleClick() {
+		goto(resolve(`/tickets/${ticket.id}`));
+	}
 </script>
 
 <Card class="cursor-grab gap-4 bg-background p-4 select-none active:cursor-grabbing">
@@ -19,9 +25,13 @@
 				{priority.label}
 			</Badge>
 		</div>
-		<p class=" text-sm font-medium">
+		<button
+			type="button"
+			onclick={handleClick}
+			class="w-fit cursor-pointer text-left text-sm font-medium hover:underline"
+		>
 			{ticket.title}
-		</p>
+		</button>
 	</CardHeader>
 	<CardContent class="p-0">
 		<p class="text-xs text-muted-foreground">{ticket.description}</p>
